47b1525553bb99273e24aa3c2bafc0440a550c8c
tvoyo <artesi4ek.kh@gmail.com>
2022-04-17 18:23:50 +0300

    Initial Commit

---
+++ Artem's Card/Artem_s_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artem_s_CardApp.swift
+//  Artem's Card
+//
+//  Created by  TVOYO on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Artem_s_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
